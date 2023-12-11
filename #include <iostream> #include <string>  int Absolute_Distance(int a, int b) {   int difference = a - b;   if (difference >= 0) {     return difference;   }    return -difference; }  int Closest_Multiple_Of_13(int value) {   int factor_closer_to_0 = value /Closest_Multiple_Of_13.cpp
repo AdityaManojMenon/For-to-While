13bd3a1954fad43753976e15c6c4f6c710b3b3f6
Aditya Manoj Menon <143031124+AdityaManojMenon@users.noreply.github.com>
2023-12-10 23:58:04 -0500

    Create Closest_Multiple_Of_13.cpp
    Write a function that when given an integer finds the closest to 13

---
+++ #include <iostream> #include <string>  int Absolute_Distance(int a, int b) {   int difference = a - b;   if (difference >= 0) {     return difference;   }    return -difference; }  int Closest_Multiple_Of_13(int value) {   int factor_closer_to_0 = value /Closest_Multiple_Of_13.cpp
@@ -0,0 +1,40 @@
+//working file
+
+#include <iostream>
+#include <string>
+
+int Absolute_Distance(int a, int b) {
+  int difference = a - b;
+  if (difference >= 0) {
+    return difference;
+  } 
+  return -difference;
+}
+
+int Closest_Multiple_Of_13(int value) {
+  int factor_closer_to_0 = value / 13;
+  int factor_farther_from_0 = factor_closer_to_0;
+  if (value > 0) {
+    ++factor_farther_from_0;
+  } else {
+    --factor_farther_from_0;
+  }
+  int multiple_closer_to_0 = factor_closer_to_0 * 13;
+  int multiple_farther_from_0 = factor_farther_from_0 * 13;
+  if (Absolute_Distance(multiple_closer_to_0, value) < 
+      Absolute_Distance(multiple_farther_from_0, value)) {
+    return multiple_closer_to_0;
+  }
+  return multiple_farther_from_0;
+}
+
+
+int Triple(int value) {
+  return value * 3;
+}
+
+int main() {
+  int value;
+  std::cin >> value;
+  std::cout << value * 2;
+}
